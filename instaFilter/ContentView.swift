14787bbccbccbd05039482255cae52b92bac4ba0
mich balkany <askmitchb@gmail.com>
2023-12-25 12:11:26 -0500

    Initial Commit

---
+++ instaFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  instaFilter
+//
+//  Created by Mich balkany on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ instaFilter/instaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaFilterApp.swift
+//  instaFilter
+//
+//  Created by Mich balkany on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct instaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
