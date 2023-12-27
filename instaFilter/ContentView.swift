8826a61d06e4173f8cf0fba7d196646dcbd1c2ee
mich balkany <askmitchb@gmail.com>
2023-12-27 10:22:49 -0500

    added method to load selected images

--- instaFilter/ContentView.swift
+++ instaFilter/ContentView.swift
@@ -5,17 +5,60 @@
 //  Created by Mich balkany on 12/25/23.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntesity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description:Text("Tap to import a photo"))
+                    }
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntesity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                    
+                    
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
-        .padding()
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // more code to come
+        }
     }
 }
 
